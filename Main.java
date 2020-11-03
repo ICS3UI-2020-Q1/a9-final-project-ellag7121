6961bb1a79f6bc344d2483ca4881fa6f564e3401
ellag7121 <ellag7121@wrdsb.ca>
2020-11-03 14:23:31 +0000

    set up the basic GUI layout

--- Main.java
+++ Main.java
@@ -4,23 +4,59 @@ import java.awt.event.*;
 
 public class Main implements Runnable, ActionListener{
 
-  // Class Variables  
+  // Class Variables 
+  JPanel mainPanel;
+
+  JTextField inputField;
+  JTextArea outputField;
+
+  JTextArea statDisplay;
+  JTextArea inventoryDisplay;
+  //placeholder until my image is made
+  JTextArea mapDisplay;
   
 
 
   // Method to assemble our GUI
   public void run(){
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
-    JFrame frame = new JFrame("Title");
+    JFrame frame = new JFrame("Sock Quest");
     // Makes the X button close the program
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // makes the windows 800 pixel wide by 600 pixels tall
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
     
+    //set up the main panel
 
+    mainPanel = new JPanel();
+    mainPanel.setLayout(null); 
+
+    //set up the components
+    inputField = new JTextField();
+    outputField = new JTextArea();
+
+    statDisplay = new JTextArea();
+    inventoryDisplay = new JTextArea();
+    mapDisplay = new JTextArea();
+
+    //set up component locations
+    outputField.setBounds(10,10,780,285);
+    inputField.setBounds(10,305,385,20);
+    inventoryDisplay.setBounds(10,335,187,255);
+    statDisplay.setBounds(208,335,187,255);
+    mapDisplay.setBounds(405,305,385,285);
+
+    //add components to the main panel
+    mainPanel.add(outputField);
+    mainPanel.add(inputField);
+    mainPanel.add(inventoryDisplay);
+    mainPanel.add(statDisplay);
+    mainPanel.add(mapDisplay);
+
+    //add main panel to the frame
+    frame.add(mainPanel);
   }
 
   // method called when a button is pressed
